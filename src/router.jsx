b7c5218f2c7f002ed258a6c6ffd5bd3f39f74491
Augusto Oliveira <augusto.oliveira.br@outlook.com>
2025-09-23 18:47:18 -0300

    Implemented file upload

--- src/router.jsx
+++ src/router.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home/index.jsx";
-import { DexUpload } from "./pages/Dex/UploadFIle/index.jsx";
+import { FileHandler } from "./pages/Dex/UploadFIle/index.jsx";
 import { PageNotFound } from "./pages/PageNotFound/index.jsx";
 import Login from "./pages/Login/index.jsx";
 import ProtectedRouter from "./protectedRouter.jsx";
@@ -10,7 +10,7 @@ const Router = () => {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route element={<ProtectedRouter />}>
-        <Route path="/dexUpload" element={<DexUpload />} />
+        <Route path="/dexUpload" element={<FileHandler />} />
       </Route>
       <Route path="*" element={<PageNotFound />} />
     </Routes>
