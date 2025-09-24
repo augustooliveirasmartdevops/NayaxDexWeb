f0cda290cffe03e29cf32234083771c86cc7fb53
Augusto Oliveira <augusto.oliveira.br@outlook.com>
2025-09-23 23:50:14 -0300

    Code cleanup

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,31 +1,11 @@
-import { useContext } from "react";
-import { AuthContext } from "../../contexts/AuthProvider/context";
-import { Button, Typography } from "@mui/material";
-import { SignInButton } from "../../components/SignInButton";
-import { Link } from "react-router-dom";
+import { Typography } from "@mui/material";
 
 export const Home = () => {
-  const { userState } = useContext(AuthContext);
-  const { isAuthenticated } = userState;
   return (
     <>
       <Typography variant="h3" component="h2">
         Nayax
       </Typography>
-
-      {!isAuthenticated ? (
-        <SignInButton title="Sing in" navigateTo="/login" />
-      ) : (
-        <Button
-          variant="outlined"
-          color="default"
-          sx={{ mt: 3, mb: 2, mr: 2 }}
-          component={Link}
-          to={"/dexupload"}
-        >
-          Welcome! Go to Dex Upload
-        </Button>
-      )}
     </>
   );
 };
